7ad48d4aca3def2d92c9685213303c0108c282e2
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-01 23:56:14 +0100

    adding some colors and changng opacity

--- IntermediatePythonForDataScience/matplotlib.py
+++ IntermediatePythonForDataScience/matplotlib.py
@@ -27,7 +27,7 @@ np_pop = np.array(pop)
 np_pop = np_pop*2
 
 # Update: set s argument to np_pop
-plt.scatter(gdp_cap, life_exp, s = np_pop)
+plt.scatter(x = gdp_cap, y = life_exp, s = np_pop, c = col, alpha=0.8)
 
 # Previous customizations
 plt.xscale('log') 
