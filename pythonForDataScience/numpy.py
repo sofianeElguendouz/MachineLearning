5ed6365ff6455aeb6ac8de4eabc0f077082a31d0
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-02 21:59:11 +0100

    create numpy test file with some plots

---
+++ pythonForDataScience/numpy.py
@@ -0,0 +1,44 @@
+# Import the libraries
+
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+%matplotlib inline  
+#########################################
+
+# Plotting functions
+
+def Plotvec1(u, z, v):
+    
+    ax = plt.axes()
+    ax.arrow(0, 0, *u, head_width=0.05, color='r', head_length=0.1)
+    plt.text(*(u + 0.1), 'u')
+    
+    ax.arrow(0, 0, *v, head_width=0.05, color='b', head_length=0.1)
+    plt.text(*(v + 0.1), 'v')
+    ax.arrow(0, 0, *z, head_width=0.05, head_length=0.1)
+    plt.text(*(z + 0.1), 'z')
+    plt.ylim(-2, 2)
+    plt.xlim(-2, 2)
+
+def Plotvec2(a,b):
+    ax = plt.axes()
+    ax.arrow(0, 0, *a, head_width=0.05, color ='r', head_length=0.1)
+    plt.text(*(a + 0.1), 'a')
+    ax.arrow(0, 0, *b, head_width=0.05, color ='b', head_length=0.1)
+    plt.text(*(b + 0.1), 'b')
+    plt.ylim(-2, 2)
+    plt.xlim(-2, 2)
+    
+    ################################################################
+    u = np.array([1, 0])
+    v = np.array([0, 1])
+    z = u + v
+    Plotvec1(u, z, v)
+    
+    
+    a = np.array([1, 2, 3, 4, 5])
+    b = np.array([1, 0, 1, 0, 1])
+    Plotvec2(a,b)c = a*b
