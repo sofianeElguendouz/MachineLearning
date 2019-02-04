28301e2f2b4082693a7538c778b97ce897dd1b5a
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-04 21:40:25 +0100

    plot CYLINDER vs the Emission
    plot CYLINDER vs the Emission, to see how linear is their relation

---
+++ MLwithPython/regression.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+!wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv
+
+df = pd.read_csv("FuelConsumption.csv")
+# take a look at the dataset
+df.head()
+
+# summarize the data
+df.describe()
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS,cdf.CO2EMISSIONS, color = 'red')
+plt.xlabel("CYLINDERS")
+plt.ylabel("CO2EMISSIONS")
+plt.show()
