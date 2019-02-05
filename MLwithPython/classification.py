190ca98ca1ca7d57de674029341462aaf904bc43
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 21:18:21 +0100

    different k values testing

--- MLwithPython/classification.py
+++ MLwithPython/classification.py
@@ -41,3 +41,38 @@ yhat[0:5]
 from sklearn import metrics
 print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
 print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+#different k 
+# write your code here
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+for k in range(20,40):
+    knnModel = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+    y_predicted = knnModel.predict(X_test)
+    print("Train set accuracy = ", metrics.accuracy_score(y_train, knnModel.predict(X_train)))
+    print("Test set accuracy = ", metrics.accuracy_score(y_test, y_predicted))
+    print("-----------------------------------------------------------------------------------")
+    
+
+Ks = 10
+mean_acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+ConfustionMx = [];
+for n in range(1,Ks):
+    #Train Model and Predict  
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat=neigh.predict(X_test)
+    mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+    std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+mean_acc
+
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show()
+
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1) 
