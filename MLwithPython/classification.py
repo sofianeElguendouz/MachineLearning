99903d4566c00ee151d722bd4bd70ae2ce99fc9f
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 20:47:01 +0100

    preparing data, features set & labels

---
+++ MLwithPython/classification.py
@@ -0,0 +1,22 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+%matplotlib inline
+
+!wget -O teleCust1000t.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/teleCust1000t.csv
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
+df.columns
+
+#Features set
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+X[0:5]
+#Labels
+y = df['custcat'].values
+y[0:5]
+
