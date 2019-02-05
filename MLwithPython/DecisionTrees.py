3544d4df9ece67ffc8836a38a0e67630cefe0b94
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 22:43:39 +0100

    add results visualization & evaluation code

--- MLwithPython/DecisionTrees.py
+++ MLwithPython/DecisionTrees.py
@@ -31,3 +31,26 @@ predTree = drugTree.predict(X_testset)
 
 print (predTree [0:5])
 print (y_testset [0:5])
+
+#evaluation
+from sklearn import metrics
+import matplotlib.pyplot as plt
+print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_testset, predTree))
+
+#Visualization
+from sklearn.externals.six import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
+%matplotlib inline 
+
+dot_data = StringIO()
+filename = "drugtree.png"
+featureNames = my_data.columns[0:5]
+targetNames = my_data["Drug"].unique().tolist()
+out=tree.export_graphviz(drugTree,feature_names=featureNames, out_file=dot_data, class_names= np.unique(y_trainset), filled=True,  special_characters=True,rotate=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png(filename)
+img = mpimg.imread(filename)
+plt.figure(figsize=(100, 200))
+plt.imshow(img,interpolation='nearest')
