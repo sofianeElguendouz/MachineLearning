e3b03bdbce56c19a45fadd8508444faf3a4adbac
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 23:37:37 +0100

    data importation & preprocessing

---
+++ MLwithPython/logisticRegression.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+%matplotlib inline 
+import matplotlib.pyplot as plt
+
+!wget -O ChurnData.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/ChurnData.csv
+churn_df = pd.read_csv("ChurnData.csv")
+churn_df.head()
+
+churn_df = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip',   'callcard', 'wireless','churn']]
+churn_df['churn'] = churn_df['churn'].astype('int')
+churn_df.head()
+print(churn_df.shape)
+print(churn_df.columns.values)
+
+X = np.asarray(churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']])
+y = np.asarray(churn_df['churn'])
+
+#preprocessing
+from sklearn import preprocessing
+X = preprocessing.StandardScaler().fit(X).transform(X)
+
+#train test split 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
