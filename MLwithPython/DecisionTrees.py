9c34b8d042dcf640d300a82236442645a82e8ebf
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 22:26:34 +0100

    preprocessing of data

---
+++ MLwithPython/DecisionTrees.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+my_data = pd.read_csv("drug200.csv", delimiter=",")
+my_data[0:5]
+
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
