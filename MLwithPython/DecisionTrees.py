ccc00525d62c6a8bfbe0b8a28dbd05726b1a6168
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-05 22:36:05 +0100

    create, fit, predict with decision tree classifier

--- MLwithPython/DecisionTrees.py
+++ MLwithPython/DecisionTrees.py
@@ -9,15 +9,25 @@ from sklearn import preprocessing
 le_sex = preprocessing.LabelEncoder()
 le_sex.fit(['F','M'])
 X[:,1] = le_sex.transform(X[:,1]) 
-
-
 le_BP = preprocessing.LabelEncoder()
 le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
 X[:,2] = le_BP.transform(X[:,2])
-
-
 le_Chol = preprocessing.LabelEncoder()
 le_Chol.fit([ 'NORMAL', 'HIGH'])
 X[:,3] = le_Chol.transform(X[:,3]) 
 
-X[0:5]
+y = my_data["Drug"]
+from sklearn.model_selection import train_test_split
+X_trainset, X_testset, y_trainset, y_testset = train_test_split(X, y, test_size=0.3, random_state=3)
+
+print(X_trainset.shape)
+print(y_trainset.shape)
+print(X_testset.shape)
+print(y_testset.shape)
+
+drugTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+drugTree.fit(X_trainset,y_trainset)
+predTree = drugTree.predict(X_testset)
+
+print (predTree [0:5])
+print (y_testset [0:5])
