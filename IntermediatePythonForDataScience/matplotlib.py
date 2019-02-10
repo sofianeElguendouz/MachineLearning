d327ed46ad37703680d866e1c3a477c8ee4725f1
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-10 22:33:51 +0100

    plt title modification

--- IntermediatePythonForDataScience/matplotlib.py
+++ IntermediatePythonForDataScience/matplotlib.py
@@ -33,7 +33,7 @@ plt.scatter(x = gdp_cap, y = life_exp, s = np_pop, c = col, alpha=0.8)
 plt.xscale('log') 
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
-plt.title('World Development in 2007')
+plt.title('World Development <2007>')
 plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
 
 # Additional customizations
