e8318f3e33486f0efa611f344471d33ad419afaf
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-11 22:00:38 +0100

    HC with scipy

--- MLwithPython/Hierarchical-Clustering.py
+++ MLwithPython/Hierarchical-Clustering.py
@@ -74,3 +74,39 @@ pdf = pdf.reset_index(drop=True)
 print ("Shape of dataset after cleaning: ", pdf.size)
 pdf.head(5)
 
+#feature selection
+featureset = pdf[['engine_s',  'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap', 'mpg']]
+#normalization
+from sklearn.preprocessing import MinMaxScaler
+x = featureset.values #returns a numpy array
+min_max_scaler = MinMaxScaler()
+feature_mtx = min_max_scaler.fit_transform(x)
+feature_mtx [0:5]
+#clustering scipy
+import scipy
+leng = feature_mtx.shape[0]
+D = scipy.zeros([leng,leng])
+for i in range(leng):
+    for j in range(leng):
+        D[i,j] = scipy.spatial.distance.euclidean(feature_mtx[i], feature_mtx[j])
+        
+import pylab
+import scipy.cluster.hierarchy
+Z = hierarchy.linkage(D, 'complete')
+
+from scipy.cluster.hierarchy import fcluster
+max_d = 3
+clusters = fcluster(Z, max_d, criterion='distance')
+
+from scipy.cluster.hierarchy import fcluster
+k = 5
+clusters = fcluster(Z, k, criterion='maxclust')
+clusters
+
+fig = pylab.figure(figsize=(18,50))
+def llf(id):
+    return '[%s %s %s]' % (pdf['manufact'][id], pdf['model'][id], int(float(pdf['type'][id])) )
+    
+dendro = hierarchy.dendrogram(Z,  leaf_label_func=llf, leaf_rotation=0, leaf_font_size =12, orientation = 'right')
+clusters
+
