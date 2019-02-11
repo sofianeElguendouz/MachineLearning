b13293b4de6885413617310f1cf96dec08cca3b2
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-02-11 21:22:41 +0100

    set up k-means clusterning and fit it with custom

--- MLwithPython/k-means.py
+++ MLwithPython/k-means.py
@@ -75,5 +75,33 @@ from sklearn.preprocessing import StandardScaler
 X = df.values[:,1:]
 X = np.nan_to_num(X)
 Clus_dataSet = StandardScaler().fit_transform(X)
+#set up k-means model and fit it with the customer dataset
+clusterNum = 3
+k_means = KMeans(init = "k-means++", n_clusters = clusterNum, n_init = 12)
+k_means.fit(X)
+#assign labels to each row in dataframe
+df["Clus_km"] = labels
+#check centroides 
+df.groupby('Clus_km').mean()
+#distrub of customers based on age & income
+area = np.pi * ( X[:, 1])**2  
+plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(np.float), alpha=0.5)
+plt.xlabel('Age', fontsize=18)
+plt.ylabel('Income', fontsize=16)
+plt.show()
+#with 3D
+from mpl_toolkits.mplot3d import Axes3D 
+fig = plt.figure(1, figsize=(8, 6))
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+plt.cla()
+# plt.ylabel('Age', fontsize=18)
+# plt.xlabel('Income', fontsize=16)
+# plt.zlabel('Education', fontsize=16)
+ax.set_xlabel('Education')
+ax.set_ylabel('Age')
+ax.set_zlabel('Income')
+ax.scatter(X[:, 1], X[:, 0], X[:, 3], c= labels.astype(np.float))
 
 
