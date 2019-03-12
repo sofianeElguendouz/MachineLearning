f43e1b6a8de9bef9c753bd61d4275e65b7250de6
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-03-12 21:49:47 +0100

    one step weight update

--- DeepLearningInPython/optimization.py
+++ DeepLearningInPython/optimization.py
@@ -69,3 +69,30 @@ slope = 2 * input_data * error
 
 # Print the slope
 print(slope)
+###############################################
+# Set the learning rate: learning_rate
+learning_rate = 0.01
+
+# Calculate the predictions: preds
+preds = (weights * input_data).sum()
+
+# Calculate the error: error
+error = preds - target
+
+# Calculate the slope: slope
+slope = 2 * input_data * error
+
+# Update the weights: weights_updated
+weights_updated = weights - slope * learning_rate
+
+# Get updated predictions: preds_updated
+preds_updated = (input_data * weights_updated).sum()
+
+# Calculate updated error: error_updated
+error_updated = target - preds_updated
+
+# Print the original error
+print(error)
+
+# Print the updated error
+print(error_updated)
