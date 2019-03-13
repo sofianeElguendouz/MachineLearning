877514a6dac4cc7b272d2f23f8919f64d9ce120b
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-03-13 18:41:26 +0100

    use the model to predict survival true prob

--- DeepLearningInPython/model.py
+++ DeepLearningInPython/model.py
@@ -66,3 +66,26 @@ model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['ac
 
 # Fit the model
 model.fit(predictors, target)
+
+##################################################
+# use the model to predict survival true probabilities 
+##################################################
+
+
+# Specify, compile, and fit the model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='sgd', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(predictors, target)
+
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:,1]
+
+# print predicted_prob_true
+print(predicted_prob_true)
